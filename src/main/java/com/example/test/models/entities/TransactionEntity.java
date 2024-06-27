e8bab52803a3c2ace7d9c0afebe8b30fdca8b00c
Davide <davidelaureti54@gmail.com>
2024-06-27 19:38:11 +0900

    Initial commit, working template

--- src/main/java/com/example/test/models/entities/TransactionEntity.java
+++ src/main/java/com/example/test/models/entities/TransactionEntity.java
@@ -3,7 +3,6 @@ package com.example.test.models.entities;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-//クラスの全てのフィールドを引数にとる。
 @AllArgsConstructor
 @Data
 public class TransactionEntity {
